fc0a44f1e4fcc7c8277166c9181d3fddd361ca94
MariamYoussef23 <mariamyoussef@aucegypt.edu>
2022-08-11 15:07:07 +0200

    login router first part

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,19 +1,49 @@
 import { Router } from "express";
-
+import bcrypt from "bcryptjs";
+import { User } from "../entities/user";
 
 const router = Router();
 
-router.post('/signup', async (req,res) => {
-    try{
-        const {firstname, lastName, email, password} = req.body
-        if(password.length > 8) {
-            return res.status(400).json({message: 'Password must be at least 8 characters '})
-        }
-    
-    }catch (error){
-        res.status(500).json({ message: error });
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json({ data: users });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+});
+
+router.post("/signup", async (req, res) => {
+  try {
+    const { firstname, lastName, email, password } = req.body;
+    if (password.length > 8) {
+      return res
+        .status(400)
+        .json({ message: "Password must be at least 8 characters " });
     }
-})
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+});
 
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-export default router
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return res.status(400).json({ message: "user not found" });
+    }
+
+    const correctPassword = await bcrypt.compare(password, user.password);
+    if (!correctPassword) {
+      return res.status(400).json({ message: "wrong password" });
+    }
+
+    res.json({ data: user });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
+});
+
+export default router;
