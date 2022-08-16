ae7cf9718b1e36c40bbe910dadf49324e3734048
MariamYoussef23 <mariamyoussef@aucegypt.edu>
2022-08-16 10:05:59 +0200

    create a new message route

--- src/routes/chats.ts
+++ src/routes/chats.ts
@@ -1,15 +1,34 @@
 import { Router } from "express";
+import { Chat } from "../entities/chat";
+import { Message } from "../entities/message";
+import { User } from "../entities/user";
+import { RequestAuth, UserType } from "../types";
 import { middleware } from "./middleware";
 const router = Router();
 
+router.post("/message", middleware, async (req: RequestAuth, res) => {
+  try {
+    const user = req.user
+    const { body, chatId} = req.body;
+    const chat = await Chat.findOne({ where: { id: chatId } });
+    if (!chat) {
+      return res.status(400).send({ message: "no chat found" });
+    }
+    const message = Message.create({
+      body,
+      chat,
+      user:req.user
+    });
+    
+  } catch (error) {
 
+  }
+});
 
 export default router;
 
-
-
-//when a user is logged in 
-    //all chats (fahad)
-    // all messages within a chat (mariam)
-    //create a new message  (fahad)
-    //create a new chat (mariam)
+//when a user is logged in
+//all chats (fahad)
+// all messages within a chat (mariam)
+//create a new message  (fahad)
+//create a new chat (mariam)
